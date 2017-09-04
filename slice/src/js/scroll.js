c766de41378275105a3976a1d68cca5eb24606c8
sveta4acova <sshurlachakova@oneclickdev.com>
2017-09-04 23:33:25 +0300

    created adjacent links block, footer

--- slice/src/js/scroll.js
+++ slice/src/js/scroll.js
@@ -1,9 +1,25 @@
 $(function(){
+	addedBgHeader();
+
 	$(window).scroll(function(){
-		var scrollValue = $(document).scrollTop();
-		scrollValue > 40 ? GLOBALS.$header.addClass('scrolled') : GLOBALS.$header.removeClass('scrolled');
+		addedBgHeader();
 	});
 
-	$('.news-container').niceScroll({cursorcolor:"#8ecdff"});
-	$('.content-block').niceScroll({cursorcolor:"#8ecdff"});
+	$('.news-container').niceScroll({cursorcolor:"#8ecdff", grabcursorenabled: false});
+	$('.content-block').niceScroll({cursorcolor:"#8ecdff", grabcursorenabled: false});
+
+	$('.animate-anchor').click(function(){
+		var $this = $(this),
+			anchor = $this.attr('data-id'),
+			$scrollToBlock = $(anchor);
+
+		$scrollToBlock.animatescroll({
+			scrollSpeed: 1500
+		});
+	});
+
+	function addedBgHeader(){
+		var scrollValue = $(document).scrollTop();
+		scrollValue > 40 ? GLOBALS.$header.addClass('scrolled') : GLOBALS.$header.removeClass('scrolled');
+	}
 });
