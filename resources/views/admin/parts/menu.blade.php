2a976dbd9697798783b0341d4429649c1c430827
zheoreh <oeswww@gmail.com>
2017-09-17 02:04:59 +0300

    Cleaned up; Implemented admin name in the header; Implemented dynamic lang for html tag; Refactored link in the admin menu;

--- resources/views/admin/parts/menu.blade.php
+++ resources/views/admin/parts/menu.blade.php
@@ -9,9 +9,10 @@
             </div>
         @else
             <ul class="navigation">
-                <li class="navigation__item"><a class="item__link" href="/admin/pages">Страницы</a></li>
-                <li class="navigation__item"><a class="item__link" href="/admin/team">Команда</a></li>
-                <li class="navigation__item"><a class="item__link" href="/admin/treatments">Обращения</a></li>
+                <li class="navigation__item"><a class="item__link" href="{{ url('/admin/pages') }}">Страницы</a></li>
+                <li class="navigation__item"><a class="item__link" href="{{ url('/admin/team') }}">Команда</a></li>
+                <li class="navigation__item"><a class="item__link" href="{{ url('/admin/treatments') }}">Обращения</a></li>
+                <li class="navigation__item" style="margin-left: 460px; color: #fff;">Привет, {{ Auth::user()->name }}</li>
             </ul>
         @endif
         <a href="{{ route('logout-admin') }}" class="logout">Выйти</a>
