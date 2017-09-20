385780ec2610036948896eea2f4392e24993d180
zheoreh <oeswww@gmail.com>
2017-09-20 03:34:08 +0300

    implemented seeder for seo records

--- database/seeds/SeoRecordsTableSeeder.php
+++ database/seeds/SeoRecordsTableSeeder.php
@@ -11,6 +11,15 @@ class SeoRecordsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('seo_records')->insert([
+                'title' => 'Присоединение к ЕИРЦ',
+                'text' => 'НО РФ КРМД РК присоединился к системе <a href="/">ЕИРЦ</a>. Появиласьвозможность контроля платежей с помощью <a href="/">Личного Кабинета</a>',
+                'user_id' => 1,
+                'updated_user_id' => 1,
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ]);
+        }
     }
 }
