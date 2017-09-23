8bc8d6bd9e58992dbd01e6c279f9aba3e177cfdf
zheoreh <oeswww@gmail.com>
2017-09-23 22:53:55 +0300

    reverted

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -1,18 +1,18 @@
 @extends('main.layout')
 
 @section('content')
-    {{--<div class="headers-container">--}}
-        {{--@include('main.parts.search')--}}
-        {{--@include('main.parts.topbar')--}}
-        {{--@include('main.parts.topmenu')--}}
-    {{--</div>--}}
-    {{--<section class="main-block" id="top">--}}
-        {{--@include('main.parts.covers')--}}
-    {{--</section>--}}
+    <div class="headers-container">
+        @include('main.parts.search')
+        @include('main.parts.topbar')
+        @include('main.parts.topmenu')
+    </div>
+    <section class="main-block" id="top">
+        @include('main.parts.covers')
+    </section>
     <section class="news-block">
-        {{--@include('main.parts.sliders')--}}
+        @include('main.parts.sliders')
         @include('main.parts.seo-records')
     </section>
-{{--    @include('main.parts.news')--}}
+    @include('main.parts.news')
     @include('main.parts.gov-resources')
 @endsection
