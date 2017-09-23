e1690986339a92b9fd4d09711fb59f86140d37a6
zheoreh <oeswww@gmail.com>
2017-09-23 22:49:36 +0300

    test 3

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -9,10 +9,10 @@
     {{--<section class="main-block" id="top">--}}
         {{--@include('main.parts.covers')--}}
     {{--</section>--}}
-    {{--<section class="news-block">--}}
+    <section class="news-block">
         {{--@include('main.parts.sliders')--}}
-        {{--@include('main.parts.seo-records')--}}
-    {{--</section>--}}
-    @include('main.parts.news')
+        @include('main.parts.seo-records')
+    </section>
+{{--    @include('main.parts.news')--}}
     @include('main.parts.gov-resources')
 @endsection
