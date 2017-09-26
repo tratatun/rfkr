df48f4ce3441281a3121cef86a68b9fa77c991ce
zheoreh <oeswww@gmail.com>
2017-09-26 23:31:11 +0300

    implemented class error for the treatment form. Removed fields name from error messages

--- resources/views/main/treatments/create.blade.php
+++ resources/views/main/treatments/create.blade.php
@@ -9,7 +9,7 @@
               class="message-send__form">
             {{csrf_field()}}
             <div class="form-group-block form-group-block_type">
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('type') ? 'error' : '' }}">
                     <label class="form-group__label required">Тип обращения</label>
                     <select class="form-group__select" name="type">
                         <option class="select__option" value=""></option>
@@ -25,7 +25,7 @@
             </div>
             <div class="form-group-block form-group-block_user">
                 <div class="form-row">
-                    <div class="form-group col-3">
+                    <div class="form-group col-3 {{ $errors->has('lastname') ? 'error' : '' }}">
                         <label class="form-group__label required">Фамилия</label>
                         <input class="form-group__input" type="text" name="lastname" value="{{ old('lastname') }}">
                         <span class="form-group__tip">Используйте настоящее имя</span>
@@ -33,14 +33,14 @@
                             <span class="error-message">{{ $errors->first('lastname') }}</span>
                         @endif
                     </div>
-                    <div class="form-group col-3">
+                    <div class="form-group col-3 {{ $errors->has('firstname') ? 'error' : '' }}">
                         <label class="form-group__label required">Имя</label>
                         <input class="form-group__input" type="text" name="firstname" value="{{ old('firstname') }}">
                         @if ($errors->has('firstname'))
                             <span class="error-message">{{ $errors->first('firstname') }}</span>
                         @endif
                     </div>
-                    <div class="form-group col-3">
+                    <div class="form-group col-3 {{ $errors->has('patronymic') ? 'error' : '' }}">
                         <label class="form-group__label">Отчество</label>
                         <input class="form-group__input" type="text" name="patronymic" value="{{ old('patronymic') }}">
                         @if ($errors->has('patronymic'))
@@ -49,7 +49,7 @@
                     </div>
                 </div>
                 <div class="form-row">
-                    <div class="form-group col-3">
+                    <div class="form-group col-3 {{ $errors->has('gender') ? 'error' : '' }}">
                         <label class="form-group__label">Пол</label>
                         <select class="form-group__select" name="gender">
                             <option class="select__option" value=""></option>
@@ -63,7 +63,7 @@
                 </div>
             </div>
             <div class="form-group-block form-group-block_address">
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('address') ? 'error' : '' }}">
                     <label class="form-group__label required">Адрес дома</label>
                     <input class="form-group__input" type="text" placeholder="Например: г. Симферополь, пр. Кирова 1" name="address" value="{{ old('address') }}">
                     <span class="form-group__tip">Порядок заполнения: Город, дом, корпус</span>
@@ -73,7 +73,7 @@
                 </div>
             </div>
             <div class="form-group-block form-group-block_email">
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('email') ? 'error' : '' }}">
                     <label class="form-group__label required">Электронная почта</label>
                     <input class="form-group__input" type="text" name="email" value="{{ old('email') }}">
                     <span class="form-group__tip">Срок ответа составляет до 3-х рабочих дней</span>
@@ -85,7 +85,7 @@
             <div class="form-group-block toogle form-group-block_additionally">
                 <div class="toogle__header"><a data-role="toogle" href="#additionally-data">Дополнительные контактные данные</a></div>
                 <div class="toogle__body" id="additionally-data">
-                    <div class="form-group">
+                    <div class="form-group {{ $errors->has('post_address') ? 'error' : '' }}">
                         <label class="form-group__label">Почтовый адрес</label>
                         <input class="form-group__input" type="text" name="post_address" value="{{ old('post_address') }}">
                         <span class="form-group__tip">Индекс, Страна, Край/округ/республика, город, дом, корпус, квартира</span>
@@ -93,7 +93,7 @@
                             <span class="error-message">{{ $errors->first('post_address') }}</span>
                         @endif
                     </div>
-                    <div class="form-group">
+                    <div class="form-group {{ $errors->has('phone') ? 'error' : '' }}">
                         <label class="form-group__label">Телефон</label>
                         <input class="form-group__input" type="text" name="phone" value="{{ old('phone') }}">
                         <span class="form-group__tip">+7(123) 45-67-89</span>
@@ -104,7 +104,7 @@
                 </div>
             </div>
             <div class="form-group-block form-group-block_thematics">
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('thematic') ? 'error' : '' }}">
                     <label class="form-group__label required">Тематика</label>
                     <select class="form-group__select" name="thematic">
                         <option class="select__option" value=""></option>
@@ -118,7 +118,7 @@
                 </div>
             </div>
             <div class="form-group-block form-group-block_treatment">
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('message') ? 'error' : '' }}">
                     <label class="form-group__label required">Обращение</label>
                     <textarea class="form-group__textarea" minlength="5" maxlength="440" name="message">{{ old('message') }}</textarea>
                     <span class="form-group__tip">Количество символов ограничено. Осталось: <span>440</span></span>
@@ -130,7 +130,7 @@
             <div class="form-group-block toogle form-group-block_attach">
                 <div class="toogle__header"><a data-role="toogle" href="#attach-file">Прикрепить файл</a></div>
                 <div class="toogle__body" id="attach-file">
-                    <div class="form-group">
+                    <div class="form-group {{ $errors->has('file') ? 'error' : '' }}">
                         <label class="form-group__label">Файл с компьютера</label>
                         <div class="attach-block">
                             <div class="attach-block__left-col">
@@ -144,7 +144,7 @@
                             <span class="error-message">{{ $errors->first('file') }}</span>
                         @endif
                     </div>
-                    <div class="form-group">
+                    <div class="form-group {{ $errors->has('file_url') ? 'error' : '' }}">
                         <label class="form-group__label">Указать ссылку на облачное хранилище</label>
                         <input class="form-group__input" type="text" name="file_url" value="{{ old('file_url') }}">
                         <span class="form-group__tip">Допустимы: Yandex Disk, Google Drive, Dropbox, Облако Mail.Ru, iCloud, OneDrive</span>
@@ -155,7 +155,7 @@
                 </div>
             </div>
             <div class="form-group-block form-group-block_captcha">
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('g-recaptcha-response') ? 'error' : '' }}">
                     <label class="form-group__label required">Защита от спама</label>
                     {!! app('captcha')->display(); !!}
                     <span class="form-group__tip">Поставьте флажок</span>
