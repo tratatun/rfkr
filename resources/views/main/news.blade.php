c37d9b421c1f25cb36b038137a36327ef0cfcd2e
zheoreh <oeswww@gmail.com>
2017-09-27 00:34:14 +0300

    finished news page

--- resources/views/main/news.blade.php
+++ resources/views/main/news.blade.php
@@ -15,16 +15,12 @@
             <header class="last-news__header">
                 <h4 class="header__caption">Все Новости</h4>
             </header>
-            <div class="last-news__content">
-                <div class="content-block">
-                    @foreach ($news as $newsOne)
-                        <div class="last-new">
-                            <div class="last-new__data">{{ $newsOne->created_at->diffForHumans() }}</div>
-                            <a href="{{ url($newsOne->url()) }}" class="last-new__description">{{ $newsOne->title }}</a>
-                        </div>
-                    @endforeach
+            @foreach ($news as $newsOne)
+                <div class="last-new">
+                    <div class="last-new__data">{{ $newsOne->created_at->diffForHumans() }}</div>
+                    <a href="{{ url($newsOne->url()) }}" class="last-new__description">{{ $newsOne->title }}</a>
                 </div>
-            </div>
+            @endforeach
         </section>
     </div>
 @endsection
